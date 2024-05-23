b2b0a4e5a0b4ce6b6a4deeb88089e363cb0c74d7
Marcin Szablak <m.szablak30@gmail.com>
2024-05-23 19:48:24 +0200

    added topAppBar test functionality

--- app/src/main/java/com/example/przewodnikturystyczny/MainActivity.kt
+++ app/src/main/java/com/example/przewodnikturystyczny/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.przewodnikturystyczny
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import android.widget.Toolbar
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var topAppBar: MaterialToolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        topAppBar = findViewById(R.id.topAppBar);
+
+        topAppBar.setNavigationOnClickListener {
+            Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show()
+        }
+
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.forward -> {
+                    Toast.makeText(this, "bbbb", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else ->{
+                    false
+                }
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/przewodnikturystyczny/MainFragment.kt
+++ app/src/main/java/com/example/przewodnikturystyczny/MainFragment.kt
@@ -6,34 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 class MainFragment : Fragment(R.layout.fragment_main_window) {
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
 
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main_window, container, true)
+        return inflater.inflate(R.layout.fragment_main_window, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
