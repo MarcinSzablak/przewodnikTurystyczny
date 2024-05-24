337e9024d99d39567c865d8d174e2058c401b8a9
Marcin Szablak <m.szablak30@gmail.com>
2024-05-24 13:23:39 +0200

    Added themes support Added changing fragments functionality

--- app/src/main/java/com/example/przewodnikturystyczny/EndFragment.kt
+++ app/src/main/java/com/example/przewodnikturystyczny/EndFragment.kt
@@ -5,12 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-class InformationFragment : Fragment() {
-
+class EndFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Any setup operations can be done here
     }
 
     override fun onCreateView(
@@ -18,11 +15,7 @@ class InformationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_information, container, false)
+        return inflater.inflate(R.layout.fragment_end, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        // Perform any operations on the view after it has been created
-    }
 }
--- app/src/main/java/com/example/przewodnikturystyczny/MainActivity.kt
+++ app/src/main/java/com/example/przewodnikturystyczny/MainActivity.kt
@@ -6,25 +6,46 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import android.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var topAppBar: MaterialToolbar
+    lateinit var fragmentList: List<Fragment>
+    private var currentFragmentIndex: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        fragmentList = listOf(
+            MainFragment(),
+            InformationFragment(),
+            EndFragment()
+        )
+
         topAppBar = findViewById(R.id.topAppBar);
 
-        topAppBar.setNavigationOnClickListener {
-            Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, fragmentList[currentFragmentIndex])
+                .commit()
         }
 
+        topAppBar.setNavigationOnClickListener {
+            currentFragmentIndex = goBack(fragmentList, currentFragmentIndex)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, fragmentList[currentFragmentIndex])
+                .commit()
+        }
         topAppBar.setOnMenuItemClickListener { menuItem ->
             when(menuItem.itemId){
                 R.id.forward -> {
-                    Toast.makeText(this, "bbbb", Toast.LENGTH_SHORT).show()
+                    currentFragmentIndex = goForward(fragmentList, currentFragmentIndex)
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container_view, fragmentList[currentFragmentIndex])
+                        .commit()
                     true
                 }
                 else ->{
@@ -33,5 +54,32 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
+
+    }
+    fun goBack(list: List<Fragment>, index: Int): Int {
+        var value = 0
+
+        if(index == 0){
+            value = list.size - 1
+        }
+        else{
+            value = index - 1
+        }
+
+        return value
+    }
+
+    fun goForward(list: List<Fragment>, index: Int): Int{
+        var value = 0
+
+        if(index == list.size - 1){
+            value = 0
+        }
+        else{
+            value = index + 1
+        }
+
+        return value
     }
 }
